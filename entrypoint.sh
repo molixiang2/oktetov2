495300d7b052e0d4c21e37595a757ab7d3450543
molixiang2 <106663558+molixiang2@users.noreply.github.com>
2022-06-27 12:33:51 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-ID=ad806487-2d26-4636-98b6-ab85cc8521f7
+ID=82d5976d-df14-4b88-a478-991f10502706
 AID=64
-WSPATH=/
+WSPATH=/v2ray
 PORT=80
 
 # Write V2Ray configuration
